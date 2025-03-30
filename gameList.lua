f3787f8fdea6c3a4e1fd04423dc3c111bbb54c52
yes <shtymane@gmail.com>
2025-03-30 21:32:19 +0100

    Update gameList.lua

--- gameList.lua
+++ gameList.lua
@@ -1,2 +1,6 @@
 local Games = {
   [14202073004] = "https://raw.githubusercontent.com/musertzscripts/-/refs/heads/main/unnamedshooter.lua", -- Unnamed Shooter
+  [13772394625] = "https://raw.githubusercontent.com/musertzscripts/-/refs/heads/main/bladeball.lua", -- Blade Ball [BETA]
+}
+
+return Games
