0733cb59238516e3a1c6e805ea902b43215e28aa
yes <shtymane@gmail.com>
2025-03-30 15:18:05 +0100

    Create unnamedshooterbeta.lua

---
+++ unnamedshooterbeta.lua
@@ -0,0 +1,58 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "swag hub",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "setting up ui..",
+   LoadingSubtitle = "by kazumi (@Musertz)",
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "swag hubx"
+   },
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "gg/qgunvTDUEe", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "swag hub",
+      Subtitle = "key system",
+      Note = "join the discord to get key: gg/qgunvTDUEe ", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"swagHUB.wtu"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local MainTab = Window:CreateTab("Combat", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "Aimbot",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-Script/refs/heads/main/Aimbot%20Script%20(Without%20FOV).lua"))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Auto-Shoot [BETA]",
+   Callback = function()
+   loadstring(game:HttpGet("https://github.com/musertzscripts/dioqjefoiwejf/blob/main/beta.lua"))()
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "ESP Player",
+   Callback = function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/musertzscripts/dioqjefoiwejf/refs/heads/main/huhik.lua"))()
+   end,
+})
